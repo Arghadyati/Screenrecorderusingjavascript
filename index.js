8331f110258536902b0d334317ebc08b8c255eed
Argha <arghadyatibayen@gmail.com>
2024-04-01 11:50:59 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const button = document.querySelector("button");
+button.addEventListener("click",(event)=>{
+    console.log("clicked");
+    navigator.mediaDevices.getDisplayMedia({
+        video:true,
+        audio:true
+    }).then((stream)=>{
+        const recorder=new MediaRecorder(stream)
+
+        recorder.start()
+
+        const buffer=[]
+
+        recorder.addEventListener('dataavailable',(event)=>{
+            buffer.push(event.data)
+        })
+
+        recorder.addEventListener('stop',()=>{
+            const blob=new Blob(buffer,{
+                type:'video/mp4'
+            })
+            const a=document.createElement('a')
+            a.href=URL.createObjectURL(blob)
+            a.download="recording.mp4"
+            a.click()
+        })
+
+        setTimeout(()=>{
+            recorder.stop()
+        },100000)
+    })
+})
+   
